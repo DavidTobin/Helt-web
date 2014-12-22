0289e26262b0a6601bd7a46eeaccd2d4dfebc4fc
David Tobin <dtobin08@gmail.com>
2014-12-22 00:45:50 +0000

    Styling changes

--- app/scripts/controllers/admin/user/:userid.js
+++ app/scripts/controllers/admin/user/:userid.js
@@ -11,5 +11,24 @@ angular.module('webApp')
   .controller('AdminUserUseridCtrl', function ($scope, $routeParams, User) {
     var userId = parseInt($routeParams.userId);
 
+    function removeRole (role) {
+      $scope.user.roles = _.without($scope.user.roles, role);
+
+      _.defer(function () {
+        console.log($scope.user);
+        $scope.user.save();
+      });
+    }
+
+    function addRole () {
+      if ($scope.newRole.length) {
+        $scope.user.roles.push($scope.newRole);
+
+        $scope.user.save();
+      }
+    }
+
     $scope.user = User.one(userId).get().$object;
+    $scope.removeRole = removeRole;
+    $scope.addRole = addRole;
   });
